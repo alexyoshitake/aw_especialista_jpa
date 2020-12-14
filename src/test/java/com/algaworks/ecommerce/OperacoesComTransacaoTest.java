10cb2bb7bae7e039e03e13c43553dd5d22373b91
Alex Kazumi Yoshitake <alex.yoshitake@gmail.com>
2020-12-13 22:28:04 -0300

    2.9. Inserindo o primeiro objeto com o método persist

--- src/test/java/com/algaworks/ecommerce/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/OperacoesComTransacaoTest.java
@@ -1,5 +1,8 @@
 package com.algaworks.ecommerce;
 
+import java.math.BigDecimal;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.algaworks.model.Produto;
@@ -7,14 +10,35 @@ import com.algaworks.model.Produto;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
 	@Test
+	public void inserirOPrimeiroObjeto() {
+
+		Produto produto = new Produto();
+
+		produto.setId(2);
+		produto.setNome("Câmera Canon");
+		produto.setDescricao("A melhor definição para suas fotos!");
+		produto.setValor(new BigDecimal("5000"));
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(produto);
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNotNull(produtoVerificacao);
+
+	}
+
+	@Test
 	public void abrirEFecharATransacao() {
-		Produto produto = new Produto(); // Somente para o Método não mostrar errros
+//		Produto produto = new Produto(); // Somente para o Método não mostrar errros
 
 		entityManager.getTransaction().begin();
 
-		entityManager.persist(produto);
-		entityManager.merge(produto);
-		entityManager.remove(produto);
+//		entityManager.persist(produto);
+//		entityManager.merge(produto);
+//		entityManager.remove(produto);
 
 		entityManager.getTransaction().commit();
 
