6b42d0e64949311a381503ba366245cf7ec12326
Alex Kazumi Yoshitake <alex.yoshitake@gmail.com>
2020-12-15 23:09:59 -0300

    2.15. Exercício: implementando um CRUD

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,2 +1,6 @@
 insert into Produto (id, nome, valor, descricao) values (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
 insert into Produto (id, nome, valor, descricao) values (3, 'Câmera GoPro Hero 7', 1400.0, 'Desempenho 2x melhor.');
+
+insert into Cliente (id, nome) values (2, 'Paulo Ricardo');
+insert into Cliente (id, nome) values (3, 'Felipe da Silva');
+insert into Cliente (id, nome) values (4, 'Maria Fernanda');
---
+++ src/test/java/com/algaworks/ecommerce/PrimeiroCrudTest.java
@@ -0,0 +1,66 @@
+package com.algaworks.ecommerce;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.algaworks.model.Cliente;
+
+public class PrimeiroCrudTest extends EntityManagerTest {
+
+	@Test
+	public void inserirRegistro() {
+		Cliente cliente = new Cliente();
+
+		cliente.setId(1);
+		cliente.setNome("João da Silva Sauro");
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(cliente);
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+		Assert.assertNotNull(clienteVerificacao);
+
+	}
+
+	@Test
+	public void buscarPorIdentificador() {
+		Cliente cliente = entityManager.find(Cliente.class, 2);
+
+		Assert.assertNotNull(cliente);
+		Assert.assertEquals("Paulo Ricardo", cliente.getNome());
+	}
+
+	@Test
+	public void atualizarRegistro() {
+		Cliente cliente = entityManager.find(Cliente.class, 3);
+
+		cliente.setNome("Felipe da Silva Filho");
+
+		entityManager.getTransaction().begin();
+		entityManager.merge(cliente);
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+		Assert.assertEquals("Felipe da Silva Filho", clienteVerificacao.getNome());
+	}
+
+	@Test
+	public void removerRegistro() {
+		Cliente cliente = entityManager.find(Cliente.class, 4);
+
+		entityManager.getTransaction().begin();
+		entityManager.remove(cliente);
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+		Assert.assertNull(clienteVerificacao);
+
+	}
+}
