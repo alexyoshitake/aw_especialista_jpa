ac6df78935607f65ea88143f1eaec1efdf7c13a1
Alex Kazumi Yoshitake <alex.yoshitake@gmail.com>
2020-12-15 22:42:58 -0300

    2.14. Entendendo a diferença entre os métodos persist e merge

--- src/test/java/com/algaworks/ecommerce/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/OperacoesComTransacaoTest.java
@@ -10,6 +10,47 @@ import com.algaworks.model.Produto;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
 	@Test
+	public void mostrarDiferencaPersistMerge() {
+
+		Produto produtoPersist = new Produto();
+
+		produtoPersist.setId(5);
+		produtoPersist.setNome("Smartfone One Plus");
+		produtoPersist.setDescricao("O processador mais rápido.");
+		produtoPersist.setValor(new BigDecimal("2000"));
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(produtoPersist);
+		produtoPersist.setNome("Smartfone Two Plus");
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Produto produtoVerificacao = entityManager.find(Produto.class, produtoPersist.getId());
+		Assert.assertNotNull(produtoVerificacao);
+		
+		//
+		
+		Produto produtoMerge = new Produto();
+		
+		produtoMerge.setId(6);
+		produtoMerge.setNome("Notebook Dell");
+		produtoMerge.setDescricao("O melhor da categoria.");
+		produtoMerge.setValor(new BigDecimal("2000"));
+		
+		entityManager.getTransaction().begin();
+		produtoMerge = entityManager.merge(produtoMerge);
+		produtoMerge.setNome("Notebook Dell 2");
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Produto produtoVerificacaoMerge = entityManager.find(Produto.class, produtoMerge.getId());
+		Assert.assertNotNull(produtoVerificacaoMerge);
+
+	}
+
+	@Test
 	public void inserirObjetoComMerge() {
 
 		Produto produto = new Produto();
