924b8f4e65e193ef92238003cae0d652b09ccecb
Alex Kazumi Yoshitake <alex.yoshitake@gmail.com>
2020-12-16 03:59:52 -0300

    3.5. Mapeando enumerações com Enumerated

---
+++ src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapeamentoEnumeracoesTest.java
@@ -0,0 +1,29 @@
+package com.algaworks.ecommerce.mapeamentobasico;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.model.Cliente;
+import com.algaworks.model.SexoCliente;
+
+public class MapeamentoEnumeracoesTest extends EntityManagerTest {
+
+	@Test
+	public void testarEnum() {
+		Cliente cliente = new Cliente();
+		cliente.setId(5);
+		cliente.setNome("José Mineiro");
+		cliente.setSexo(SexoCliente.MASCULINO);
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(cliente);
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+		Assert.assertNotNull(clienteVerificacao);
+	}
+
+}
